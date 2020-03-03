53e27b86fcce83b7f7236ba63191a599ddd9bea2
Kieran Devlin <kjrdevlin@gmail.com>
2020-03-03 13:54:45 -0800

    Add linear gradient

--- js/navigation/BottomTabNav.js
+++ js/navigation/BottomTabNav.js
@@ -6,6 +6,8 @@ import FavesContainer from '../screens/Faves';
 import MapContainer from '../screens/Map';
 import ScheduleContainer from '../screens/Schedule';
 import SessionContainer from '../screens/Session';
+import {sharedScreenOptions} from './config';
+import Ionicon from 'react-native-vector-icons/Ionicons';
 
 const ScheduleStack = createStackNavigator();
 const MapStack = createStackNavigator();
@@ -13,24 +15,24 @@ const FavesStack = createStackNavigator();
 const AboutStack = createStackNavigator();
 
 const ScheduleStackScreens = props => (
-  <ScheduleStack.Navigator>
+  <ScheduleStack.Navigator screenOptions={sharedScreenOptions}>
     <ScheduleStack.Screen name="Schedule" component={ScheduleContainer} />
     <ScheduleStack.Screen name="Session" component={SessionContainer} />
   </ScheduleStack.Navigator>
 );
 const MapStackScreens = props => (
-  <MapStack.Navigator>
+  <MapStack.Navigator screenOptions={sharedScreenOptions}>
     <MapStack.Screen name="Map" component={MapContainer} />
   </MapStack.Navigator>
 );
 const FavesStackScreens = props => (
-  <FavesStack.Navigator>
+  <FavesStack.Navigator screenOptions={sharedScreenOptions}>
     <FavesStack.Screen name="Faves" component={FavesContainer} />
     <FavesStack.Screen name="Session" component={SessionContainer} />
   </FavesStack.Navigator>
 );
 const AboutStackScreens = props => (
-  <AboutStack.Navigator>
+  <AboutStack.Navigator screenOptions={sharedScreenOptions}>
     <AboutStack.Screen name="About" component={AboutContainer} />
   </AboutStack.Navigator>
 );
@@ -39,11 +41,23 @@ const BottomTabNav = createBottomTabNavigator();
 
 const BottomTabNavScrens = props => (
   <BottomTabNav.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Schedule') {
+          iconName = focused ? 'calendar-today' : 'calendar-today-outlined';
+        }
+
+        return <Ionicon name={iconName} size={size} color={color} />;
+      },
+    })}
     tabBarOptions={{
       activeTintColor: '#ffffff',
       inactiveTintColor: '#999999',
       labelStyle: {
         fontSize: 10,
+        fontFamily: 'Montserrat',
       },
       style: {
         backgroundColor: '#000000',
