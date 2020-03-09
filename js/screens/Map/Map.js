8b1912815619ae427e52a4e14934c397ad59e8e3
Kieran Devlin <kjrdevlin@gmail.com>
2020-03-09 16:32:18 -0700

    Add react-native-maps for ios and android

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
-const Map = ({navigation}) => {
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+
+const Map = () => {
+  const latlng = {
+    latitude: 49.26336,
+    longitude: -123.13813,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  };
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>I am Map screen</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{flex: 1, width: 999, height: 999}}
+        region={latlng}
+        showsUserLocation={true}>
+        <Marker coordinate={latlng} title={'R10'} description={'R10'} />
+      </MapView>
     </View>
   );
 };
